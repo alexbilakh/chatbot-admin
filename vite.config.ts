f58b7c4a6ca5602edd1324837eaf2fe34b68bb3e
be-crazy121 <supertalent121@gmail.com>
2024-03-06 13:41:37 -0500

    fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: { manifest: true, outDir: './dist'},
     plugins: [react()],
     resolve: {
         alias: {
